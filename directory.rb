060f34fd0e674d8ad6afcbbe7625aa87d82d9c54
davidmcgregor <davidjohnmcgregor@gmail.com>
2020-01-10 11:48:43 +0000

    Loading the data from the file

--- directory.rb
+++ directory.rb
@@ -31,7 +31,6 @@ end
 def print_footer
   puts "Overall, we have #{@students.count} great students"
 end
-#nothing happens until we call the methods
 
 def interactive_menu
   loop do
@@ -44,6 +43,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit"
 end
 
@@ -61,6 +61,8 @@ def process(selection)
     show_students
   when "3"
     save_students
+  when "4"
+    load_students
   when "9"
     exit
   else
@@ -74,6 +76,15 @@ def save_students
     student_data = [student[:name], student[:cohort]]
     csv_line = student_data.join(",")
     file.puts csv_line
+  end
+  file.close
+end
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+  name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort}
   end
   file.close
 end
